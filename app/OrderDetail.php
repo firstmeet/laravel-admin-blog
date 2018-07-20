b1aba051c58745874afc508f2e42566f70a17ca8
firstmeet <871609160@qq.com>
2018-07-20 18:08:46 +0800

    1
    1

--- app/OrderDetail.php
+++ app/OrderDetail.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class OrderDetail extends Model
 {
     protected $guarded=[];
+
+    public function goods()
+    {
+        return $this->belongsTo(Good::class);
+    }
 }
